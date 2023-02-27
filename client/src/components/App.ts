4cf94a0c576bfae1f0a6754f4b29438ffca74725
Adophilus <uchenna19of@gmail.com>
2023-02-27 16:38:29 +0100

    refactoerd main `App`

--- client/src/components/App.ts
+++ client/src/components/App.ts
@@ -3,52 +3,48 @@ import ChatUIElement from './ChatUI'
 import LobbyUIElement from './LobbyUI'
 import './UserDetailsModal'
 import UserDetailsModalElement from './UserDetailsModal'
-import { io, Socket } from 'socket.io-client'
+import { io } from 'socket.io-client'
+import ErrorModal from './ErrorModal'
 
 class AppElement extends HTMLElement {
-  private declare _ws: Socket
+  private _ws = io('/', { path: '/chat' })
   private _username = window.sessionStorage.getItem('username')
-  private declare _registrationModalElement: UserDetailsModalElement
-  private declare _chatUIElement: ChatUIElement
-  private declare _lobbyUIElement: LobbyUIElement
+  private _registrationModalElement = new UserDetailsModalElement()
+  private _chatUIElement = new ChatUIElement()
+  private _lobbyUIElement = new LobbyUIElement()
+  private _errorModal = new ErrorModal()
   private _chatRecepient = ''
 
   constructor() {
     super()
+    this.setAttribute("class", "block w-full px-4 mx-auto mt-8 md:mt-12 max-w-xl space-y-4")
   }
 
   connectedCallback() {
-    this._ws = io('/', { path: '/chat' })
+    this._showErrorModal('Network connection lost. Reconnecting...')
+    this._ws.on('connect', () => {
+      this._ws.on('chat-message', (message) => {
+        this._chatUIElement.addMessage(new Message(message))
+      })
 
-    this._ws.on('open', () => {
-      console.log('connected to server')
+      this._ws.on('user-joined', ({ username }: { username: string }) => {
+        this._chatUIElement.notifyUserJoined(username)
+        this._lobbyUIElement.addRecepient(username)
+      })
+
+      this._ws.on('existing-user', ({ username }: { username: string }) => {
+        this._lobbyUIElement.addRecepient(username)
+      })
+
+      this._ws.on('user-left', ({ username }: { username: string }) => {
+        this._chatUIElement.notifyUserLeft(username)
+        this._lobbyUIElement.removeRecepient(username)
+      })
+
+      this._ws.on('disconnect', () => {
+        this._showErrorModal('Network connection lost. Reconnecting...')
+      })
     })
-
-    this._ws.on('chat-message', (message) => {
-      this._chatUIElement.addMessage(new Message(message))
-    })
-
-    this._ws.on('user-joined', ({ username }: { username: string }) => {
-      this._chatUIElement.notifyUserJoined(username)
-      this._lobbyUIElement.addRecepient(username)
-    })
-
-    this._ws.on('existing-user', ({ username }: { username: string }) => {
-      this._lobbyUIElement.addRecepient(username)
-    })
-
-    this._ws.on('user-left', ({ username }: { username: string }) => {
-      this._chatUIElement.notifyUserLeft(username)
-      this._lobbyUIElement.removeRecepient(username)
-    })
-
-    this._ws.on('disconnect', () => {
-      console.log('disconnected')
-    })
-
-    this._registrationModalElement = new UserDetailsModalElement()
-    this._chatUIElement = new ChatUIElement()
-    this._lobbyUIElement = new LobbyUIElement()
 
     this._registrationModalElement.addEventListener('register', (ev) => {
       const { username } = ev.detail
@@ -59,6 +55,11 @@ class AppElement extends HTMLElement {
       this._showLobbyUI()
     })
 
+    this._errorModal.addEventListener('modal-close', () => {
+      console.warn('closing modal')
+      this._hideErrorModal()
+    })
+
     this._chatUIElement.addEventListener('send-message', (ev) => {
       const message = ev.detail.message
       this._ws.emit('chat-message', { message, recepient: this._chatRecepient })
@@ -67,6 +68,7 @@ class AppElement extends HTMLElement {
     this._lobbyUIElement.addEventListener('register-recepient', (ev) => {
       const { recepient } = ev.detail
 
+      this._fetchPreviousChatWith(recepient)
       this._chatRecepient = recepient
 
       this._hideLobbyUI()
@@ -87,11 +89,26 @@ class AppElement extends HTMLElement {
     }
   }
 
+  private _showErrorModal(message: string) {
+    this._errorModal.setAttribute("message", message)
+    this.prepend(this._errorModal)
+  }
+
+  private _hideErrorModal() {
+    this._errorModal.remove()
+  }
+
   private _registerUsername(username: string) {
     this._username = username
     window.sessionStorage.setItem('username', username)
 
     this._ws.emit('chat-register', { username })
+  }
+
+  private _fetchPreviousChatWith(username: string) {
+    this._ws.emit('chat-fetch', { recepient: username }, ({ messages }: { messages: IMessage[] }) => {
+      messages.forEach(message => this._chatUIElement.addMessage(message))
+    })
   }
 
   private _hideRegistrationModal() {
