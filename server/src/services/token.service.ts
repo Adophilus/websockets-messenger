1330760b793af4104906a5943fafe74215b5c536
Adophilus <uchenna19of@gmail.com>
2023-03-16 21:16:11 +0100

    refactored token generator

--- server/src/services/token.service.ts
+++ server/src/services/token.service.ts
@@ -3,11 +3,11 @@ import config from '../config'
 import { User } from '@prisma/client'
 
 export function generateToken(user: User) {
-  return jwt.sign({ username: user.username }, config.jwt.token, {
+  return jwt.sign({ username: user.username }, config.jwt.tokenSecret, {
     expiresIn: '1d'
   })
 }
 
 export async function verifyToken(token: string) {
-  return jwt.verify(token, config.jwt.token)
+  return jwt.verify(token, config.jwt.tokenSecret)
 }
