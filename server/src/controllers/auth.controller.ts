baaa108eb57cb32c4e71299269f739febe326b21
Adophilus <uchenna19of@gmail.com>
2023-03-16 21:15:29 +0100

    fixed bug related with returning response

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import * as TokenService from '../services/token.service'
 import * as AuthService from '../services/auth.service'
 import { prisma } from '../services/database.service'
-import { StatusCodes } from 'http-status-codes'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import { Request, Response } from 'express'
 
 export async function register(req: Request, res: Response) {
@@ -22,9 +22,10 @@ export async function register(req: Request, res: Response) {
 
     return res
       .status(StatusCodes.CREATED)
+      .json({ message: ReasonPhrases.CREATED })
   } catch (err) {
     res.locals.logger.error(err)
-    return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR })
   }
 }
 
@@ -32,8 +33,10 @@ export async function login(req: Request, res: Response) {
   const { username, password } = req.body
   const user = await prisma.user.findFirst({ where: { username } })
 
+  res.locals.logger.info(`'${username}' attempted login!`)
+
   if (!user || !AuthService.verifyUserPassword(user, password))
-    return res.sendStatus(StatusCodes.UNAUTHORIZED)
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: ReasonPhrases.UNAUTHORIZED })
 
   res.locals.logger.info(`'${user.username}' just logged in!`)
 
