a221350b4ee51bf2c6e0a49de8ab19f75a7e1694
Adophilus <uchenna19of@gmail.com>
2023-03-16 21:16:36 +0100

    code formatter

--- server/src/utils/validation.util.ts
+++ server/src/utils/validation.util.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import { StatusCodes } from 'http-status-codes'
 
-export function validationFunction (req:Request, res:Response, next:NextFunction) {
+export function validationFunction(req: Request, res: Response, next: NextFunction) {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() })
