a4c8343454ee48c992def1362df4bd5c75bfd0ae
Adophilus <uchenna19of@gmail.com>
2023-03-16 08:27:44 +0100

    added token service

---
+++ server/src/services/token.service.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+import config from '../config'
+import { User } from '@prisma/client'
+
+export function generateToken(user: User) {
+  return jwt.sign({ username: user.username }, config.jwt.token, {
+    expiresIn: '1d'
+  })
+}
+
+export async function verifyToken(token: string) {
+  return jwt.verify(token, config.jwt.token)
+}
