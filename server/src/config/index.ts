a296a038c3842bf8856400bb8fa93f1f6d1aace8
Adophilus <uchenna19of@gmail.com>
2023-03-16 08:27:15 +0100

    added config for jwt

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv'
 import ServerConfig from './server.config'
+import JwtConfig from './jwt.config'
 
 dotenv.config()
 
 const config = {
+  jwt: JwtConfig,
   server: ServerConfig
 }
 
