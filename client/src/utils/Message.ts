ac6a8f090b7bda23e5fdff19af84f0fe470a07c3
Adophilus <uchenna19of@gmail.com>
2023-04-03 11:50:40 +0100

    added media type to `Message`

--- client/src/utils/Message.ts
+++ client/src/utils/Message.ts
@@ -3,15 +3,15 @@ class Message {
   declare public sender: string
   declare public recipient: string
   declare public message: string
-  declare public image: string | null
+  declare public media: string | null
   declare public has_read: boolean
 
-  constructor({ id, sender, recipient, message, image, has_read }: Message) {
+  constructor({ id, sender, recipient, message, media, has_read }: Message) {
     this.id = id
     this.sender = sender
     this.recipient = recipient
     this.message = message
-    this.image = image
+    this.media = media
     this.has_read = has_read
   }
 }
