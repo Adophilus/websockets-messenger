3448ec448339c84f6763284522c397c1a828724c
Adophilus <uchenna19of@gmail.com>
2023-04-03 11:50:53 +0100

    added config for the frontend

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,8 +1,10 @@
 import ServerConfig from './server.config'
 import JwtConfig from './jwt.config'
 import UploadConfig from './upload.config'
+import FrontendConfig from './frontend.config'
 
 const config = {
+  frontend: FrontendConfig,
   jwt: JwtConfig,
   server: ServerConfig,
   upload: UploadConfig
