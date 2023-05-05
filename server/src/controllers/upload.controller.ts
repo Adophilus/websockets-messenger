0c6dcbcb66c7557fe140d3491846231412463327
Adophilus <uchenna19of@gmail.com>
2023-05-05 10:45:56 +0100

    added in logic for uploading files

--- server/src/controllers/upload.controller.ts
+++ server/src/controllers/upload.controller.ts
@@ -3,11 +3,40 @@ import AuthService from '../services/auth.service'
 import { prisma } from '../services/database.service'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import { Request, Response } from 'express'
+import { Logger, ILogObj } from 'tslog'
+import StorageService from '@/services/storage.service'
 
 async function uploadFiles(req: Request, res: Response) {
-  console.log(req.files)
+  const logger: Logger<ILogObj> = res.locals.logger
+
+  const fileIds: (number | null)[] = []
+
+  if (req.files) {
+    if (Array.isArray(req.files)) {
+      for (const file of req.files) {
+        const fileUrl = await StorageService.uploadFile(file.path)
+        if (fileUrl) {
+          const media = await prisma.media.create({
+            data: {
+              url: fileUrl,
+              mimetype: file.mimetype,
+              size: file.size
+            }
+          })
+          logger.trace(`Uploading file: ${file.path}`)
+          fileIds.push(media.id)
+        }
+        else {
+          logger.warn(`Failed to upload file: ${file.path}`)
+          fileIds.push(null)
+        }
+      }
+    }
+  }
+
   return res.status(StatusCodes.OK).json({
-    message: 'Uploaded successfully'
+    message: 'Uploaded successfully',
+    files: fileIds
   })
 }
 
