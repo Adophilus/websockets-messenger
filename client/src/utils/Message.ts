42cdfd0dce256e0d87ce2687e886113877fb7067
Adophilus <uchenna19of@gmail.com>
2023-05-05 12:01:45 +0100

    updated the type for Message on the client

--- client/src/utils/Message.ts
+++ client/src/utils/Message.ts
@@ -3,7 +3,7 @@ class Message {
   declare public sender: string
   declare public recipient: string
   declare public message: string
-  declare public media: string | null
+  declare public media: string[]
   declare public has_read: boolean
 
   constructor({ id, sender, recipient, message, media, has_read }: Message) {
